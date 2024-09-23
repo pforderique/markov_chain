32d6cda2b0bb65e8f35a3b4ae8e3f8f20f3dcd66
Piero Orderique <porderiq@mit.edu>
2024-09-22 17:38:57 -0700

    Add String and Add functions

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,6 +1,10 @@
 package matrix
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
 
 type Matrix struct {
 	data []float64
@@ -32,16 +36,57 @@ func (A Matrix) Get(coors ...int) float64 {
 	return A.data[data_idx]
 }
 
-// func (A Matrix) String() string {
-// 	s := "["
-// 	data_idx
+func (A Matrix) String() string {
+	// s := "["
+	// // data_idx
 
-// 	for _, dim := range A.dims {
-// 		s += "["
-// 		for idx:=0; idx<dim; idx++
-// 	}
-// }
+	
+	if len(A.dims) == 1 {
+		return fmt.Sprint(A.data)
+	}
 
-func MatrixMultiply(A *Matrix, B *Matrix) (C *Matrix) {
-	return C
+	s := "["
+	currDim := A.dims[0]
+	subMatrixStrings := []string{}
+
+	for startIdx:=0; startIdx<len(A.data); startIdx += len(A.data)/currDim {
+		subData := A.data[startIdx : startIdx+len(A.data)/currDim]
+		subMatrix := Matrix{subData, A.dims[1:]}
+		subMatrixStrings = append(subMatrixStrings, subMatrix.String())
+	}
+	s += strings.Join(subMatrixStrings, "\n")
+	s += "]"
+
+	// s := "["
+	// currDim := A.dims[len(A.dims)-1]
+	// subMatrixStrings := []string{}
+	// for startIdx:=0; startIdx<=len(A.data)-currDim; startIdx += currDim {
+	// 	subData := A.data[startIdx : startIdx+currDim]
+	// 	subMatrix := Matrix{subData, A.dims[:len(A.dims)-1]}
+	// 	// s += subMatrix.String() + "\n"
+	// 	subMatrixStrings = append(subMatrixStrings, subMatrix.String())
+	// }
+	// s += strings.Join(subMatrixStrings, ",\n")
+	// s += ",]"
+
+	return s
 }
+
+func (A Matrix) Add(B Matrix) Matrix {
+	if !reflect.DeepEqual(A.dims, B.dims) {
+		panic(fmt.Sprintf(
+			"Cannot add Matrix with dimensions %v and %v", A.dims, B.dims))
+	}
+	sumData := make([]float64, len(A.data))
+	for i := range A.data {
+		sumData[i] = A.data[i] + B.data[i]
+	}
+	return Matrix{
+		data: sumData,
+		dims: A.dims,
+	}
+}  
+
+// func matrixMultiplySimple(A *Matrix, B *Matrix) (C *Matrix) {
+// 	return C
+// }
--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -1,6 +1,7 @@
 package matrix
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -41,5 +42,57 @@ func TestGetSuccess(t *testing.T) {
 			t.Fatalf(`Test case %d failed. Expected %f, got %f`,
 				t_idx, expected, result)
 		}
+	}
+}
+
+// TestString calles Matrix.String with multidimensional coordinates
+func TestString(t *testing.T) {
+	matricies := []Matrix{
+		{[]float64{1, 2, 3, 4}, []int{4}},
+		{[]float64{1, 2, 3, 4}, []int{2, 2}},
+		{[]float64{1, 2, 3, 4, 5, 6, 7, 8}, []int{2, 2, 2}},
+	}
+
+	expecteds := []string{
+		"[1 2 3 4]",
+		"[[1 2]\n[3 4]]",
+		"[[[1 2]\n[3 4]]\n[[5 6]\n[7 8]]]",
+	}
+
+	for t_idx, matrix := range matricies {
+		expected := expecteds[t_idx]
+		result := matrix.String()
+		
+		if result != expected {
+			t.Fatalf(`Test case %d failed. Expected %s, got %s`,
+				t_idx, expected, result)
+		}
+	}
+}
+
+// TestAdd calles Matrix.Add with multidimensional coordinates
+func TestAdd(t *testing.T) {
+	A := Matrix{
+		[]float64{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		[]int{3, 3},
+	}
+
+	B := Matrix{
+		[]float64{9, 8, 7, 6, 5, 4, 3, 2, 1},
+		[]int{3, 3},
+	}
+
+	C := A.Add(B)
+	expected := Matrix{
+		[]float64{10, 10, 10, 10, 10, 10, 10, 10, 10},
+		[]int{3, 3},
+	}
+
+	if !reflect.DeepEqual(C.data, expected.data){
+		t.Fatalf(`Expected %s, got %s`, expected.String(), C.String())
+	}
+
+	if !reflect.DeepEqual(C.dims, expected.dims) {
+		t.Fatalf(`Expected dimensions %v, got %v`, expected.dims, C.dims)
 	}
 }
